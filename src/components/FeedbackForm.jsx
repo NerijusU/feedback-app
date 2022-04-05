81cd2cae054cf0b7da87953cb1c35e9009952feb
Nerijus Urbonas <nerijus.urbonas@gmail.com>
2022-04-05 18:48:10 +0200

    add context

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import RatingSelect from "./RatingSelect";
 import Card from "./shared/Card";
 import Button from "./shared/Button";
@@ -10,7 +10,16 @@ function FeedbackForm() {
   const [btnDisabled, setBtnDisabled] = useState(true);
   const [message, setMessage] = useState("");
 
-  const {addFeedback} = useContext(FeedbackContext)
+  const { addFeedback, feedbackEdit, updateFeedback } =
+    useContext(FeedbackContext);
+
+  useEffect(() => {
+    if (feedbackEdit.edit === true) {
+      setBtnDisabled(false);
+      setText(feedbackEdit.item.text);
+      setRating(feedbackEdit.item.rating);
+    }
+  }, [feedbackEdit]);
 
   const handleTextChange = (e) => {
     if (text === "") {
@@ -33,11 +42,15 @@ function FeedbackForm() {
       const newFeedback = {
         text,
         rating,
+      };
+
+      if (feedbackEdit.edit === true) {
+        updateFeedback(feedbackEdit.item.id, newFeedback);
+      } else {
+        addFeedback(newFeedback);
       }
 
-      addFeedback(newFeedback)
-
-      setText('')
+      setText("");
     }
   };
 
--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -21,6 +21,10 @@ export const FeedbackProvider = ({ children }) => {
       rating: 2,
     },
   ]);
+  const [feedbackEdit, setFeedbackEdit] = useState({
+      item: {},
+      edit: false
+  });
 
   const addFeedback = (newFeedback) => {
     newFeedback.id = uuidv4();
@@ -33,12 +37,28 @@ export const FeedbackProvider = ({ children }) => {
     }
   };
 
+  // Update feedback item
+  const updateFeedback = (id, updItem) => {
+    setFeedback(feedback.map((item) => item.id === id ? {...item, ...updItem} : item))
+  }
+
+  // Set item to be updated
+  const editFeedback = (item) => {
+    setFeedbackEdit({
+        item,
+        edit: true
+    })
+  };
+
   return (
     <FeedbackContext.Provider
       value={{
         feedback,
+        feedbackEdit,
         deleteFeedback,
-        addFeedback
+        addFeedback,
+        editFeedback,
+        updateFeedback
       }}
     >
       {children}
